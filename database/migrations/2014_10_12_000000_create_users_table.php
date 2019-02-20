eb5184cac5eb93a5a07cdf0c5dba751f40a15273
ignassta <ignasstarzinskis@gmail.com>
2019-02-20 10:25:09 +0200

    migration update

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,9 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('phone');
+            $table->string('name', 191);
+            $table->string('email',191)->unique();
+            $table->string('phone',191);
             $table->timestamps();
         });
     }
